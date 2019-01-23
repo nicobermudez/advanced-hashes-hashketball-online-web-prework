f6e53d826d2f6c546a292e81e1dd0e810c549e9a
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 17:53:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_numbers(team_name)
           x = 1
         end
       elsif att == :players && x == 1
-        att.each do |a, b|
+        value.each do |a, b|
           b.each do |c, d|
             if c == :number
               array << c
