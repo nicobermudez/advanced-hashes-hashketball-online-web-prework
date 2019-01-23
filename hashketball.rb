fab9eabd20c89fb58a66009cd6990cb233b18e0b
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 16:53:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,8 +120,9 @@ end
 def num_points_scored(player_name)
   game_hash.each do |home, data|
     data.each do |attributes, stats|
-    if stats.include?(player_name) 
-      playername[:points]
+      if stats.include?(player_name) 
+        playername[:points]
+      end
     end
   end
 end
